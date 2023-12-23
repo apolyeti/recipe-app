2ec639445d529a23d4950eee7b04c1b66c1a5a87
apolyeti <azhandarveen@gmail.com>
2023-12-22 23:35:47 -0800

    Get backend working with frontend

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/antchfx/xpath v1.2.5 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jawher/mow.cli v1.2.0 // indirect
@@ -26,6 +27,7 @@ require (
 	golang.org/x/net v0.19.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 	google.golang.org/appengine v1.6.8 // indirect
 	google.golang.org/protobuf v1.32.0 // indirect
 )
--- server.go
+++ server.go
@@ -1,31 +1,84 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-
-	"github.com/gocolly/colly"
-
-	"github.com/gocolly/colly/debug"
+	"github.com/labstack/echo/v4/middleware"
 )
 
+type Ingredient struct {
+	/*
+		json: {ingredients: [
+			{ingredient: "ingredient1", quantity: "quantity1"},
+			{ingredient: "ingredient2", quantity: "quantity2"},
+			...
+		]}
+	*/
+	// should be list of ingredients
+	Name     string `json:"ingredient"` // name of ingredient
+	Quantity string `json:"quantity"`   // quantity of ingredient
+}
+
+type Recipe struct {
+	/*
+		json: {recipe: [
+			{ingredient: "ingredient1", quantity: "quantity1"},
+			{ingredient: "ingredient2", quantity: "quantity2"},
+			...
+		]}
+	*/
+	// should be list of ingredients
+	Ingredients []Ingredient `json:"recipe"`
+}
+
+type URL struct {
+	Name string `json:"url"`
+}
+
+func getIngredients(c echo.Context) error {
+	// get url from request body
+	requestBody := new(URL)
+
+	if err := c.Bind(requestBody); err != nil {
+		return err
+	}
+
+	// get url from req body
+	// print url to console
+	url := requestBody.Name
+	fmt.Println("url:", url)
+	r := &Recipe{
+		Ingredients: []Ingredient{
+			{
+				Name:     url,
+				Quantity: "0",
+			},
+			{
+				Name:     "ingredient2",
+				Quantity: "quantity2",
+			},
+		},
+	}
+	return c.JSON(http.StatusOK, r)
+}
+
 func main() {
-	c := colly.NewCollector(colly.Debugger(&debug.LogDebugger{}))
 	e := echo.New()
+	e.Use(middleware.CORS())
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
-	// post request, get url from body and scrape it for ingredients, using regex
-	// regex will look for numbers, and measurement words, such as cups, teaspoons, etc.
-	// return a json object with the ingredients
-	e.POST("/ingredients", func(c echo.Context) error {
-		url := c.FormValue("url")
-		c.Bind(&url)
-		c.JSON(http.StatusOK, url)
-		return nil
-	})
+	e.POST("/api/ingredients", getIngredients)
+
+	// request body:
+	// method: POST
+	// headers: Content-Type: application/json
+	// body: {"url": url}
+	// for now, response body is the same as request body
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
